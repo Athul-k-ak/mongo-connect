72d4dbc6295674b4bcfeedfb82b02080a510a5e9
Athul-k-ak <athulk8888@gmail.com>
2025-02-15 14:16:16 +0530

    make ui for app

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const authenticateToken = (req,res,next)=>{
 }
 
 // ✅ Get All Products
-app.get('/products',authenticateToken, async (req, res) => {
+app.get('/products', async (req, res) => {
     try {
         const products = await Product.find();
         res.status(200).json(products);
